24d2230dd83880f2fd3bf332d72494b8fa5f340a
tifflastimosa <lastimosa.ti@northeastern.edu>
2023-02-03 18:30:25 -0800

    create axios call

---
+++ src/services/state.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+class StateData {
+
+    // parameter to be passed in is abbreviated state
+    getHouseRepsByState(state) {
+        console.log(state);
+        return axios.get(`${process.env.REACT_APP_API_BASE_URL}/house/${state}`)
+    }
+
+    getSenateMembersByState(state) {
+        console.log(state);
+        return axios.get(`${process.env.REACT_APP_API_BASE_URL}/senate/${state}`)
+    }
+
+}
+
+export default new StateData();
